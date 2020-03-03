4b7a20509a6266ee78c0befd64d7bef78e331da2
Hwanyoung Jang <>
2020-03-03 10:34:14 +0900

    Updates

---
+++ precache-manifest.b2b1d7af110307fdb608eeee00cbb91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24460ee20a084a2dc8c347239f232776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ac697d42f313a8a3161",
+    "url": "/static/js/2.67e305c7.chunk.js"
+  },
+  {
+    "revision": "8231308fcbfa148cd407b1df32ebf0c7",
+    "url": "/static/js/2.67e305c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bf284cfc26a99784f6",
+    "url": "/static/js/main.68e6b076.chunk.js"
+  },
+  {
+    "revision": "bb8b86dd05b010481105",
+    "url": "/static/js/runtime-main.8df14699.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
