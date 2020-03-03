5cc4744d39341f66d22c1bd2359d0b3485d64466
Hwanyoung Jang <>
2020-03-03 10:58:59 +0900

    Broswer Router to Hash Router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Redirect,
   Switch
@@ -15,24 +15,11 @@ export default () => (
   <Router>
     <Header />
     <Switch>
-      <Route
-        path="https://jade-veloper.github.io/nomflix/"
-        exact
-        component={Home}
-      />
-      <Route path="https://jade-veloper.github.io/nomflix/tv" component={TV} />
-      <Route
-        path="https://jade-veloper.github.io/nomflix/search"
-        component={Search}
-      />
-      <Route
-        path="https://jade-veloper.github.io/nomflix/movie/:id"
-        component={Detail}
-      />
-      <Route
-        path="https://jade-veloper.github.io/nomflix/show/:id"
-        component={Detail}
-      />
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" component={TV} />
+      <Route path="/search" component={Search} />
+      <Route path="/movie/:id" component={Detail} />
+      <Route path="/show/:id" component={Detail} />
       <Redirect from="*" to="/" />
     </Switch>
   </Router>
